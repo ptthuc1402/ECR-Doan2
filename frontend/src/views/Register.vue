f274d0f2675bc4779acfe3f3dfb57e90eb26bcd8
quoc-thinh <lquocthinh10a1@gmail.com>
2023-12-28 23:06:37 +0700

    update logp

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -2,7 +2,7 @@
 <section class="bg-gray-50 dark:bg-gray-900 mx-auto md:h-[50%] lg:py-0 mt-[85px]">
   <div class="flex flex-col items-center justify-center px-6 py-8 ">
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
+          <img src="https://cdn4.iconfinder.com/data/icons/medical-business/512/medical_help-512.png" class='w-9 h-9 mr-3'>
           BK HEALTH
       </a>
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -28,12 +28,15 @@
                       <input type="confirm-password" name="confirm-password" v-model="user.confirmPassword" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                   </div>
                   <div class="flex items-start">
-                      <div class="flex items-center h-5">
+                      <!-- <div class="flex items-center h-5">
                         <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
-                      </div>
-                      <div class="ml-3 text-sm">
+                      </div> -->
+                      <!-- <div class="ml-3 text-sm">
                         <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                      </div>
+                      </div> -->
+                       <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      You already have an account? <a href="/login" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</a>
+                      </p>
                   </div>
                 
                   <div class="form-group">
